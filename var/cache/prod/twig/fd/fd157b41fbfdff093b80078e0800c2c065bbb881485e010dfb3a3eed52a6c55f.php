b22689beab60463e473c298318311689c7e7130b
fedisadouki <36297902+fedisadouki@users.noreply.github.com>
2018-04-03 23:41:23 +0100

    Add files via upload

---
+++ var/cache/prod/twig/fd/fd157b41fbfdff093b80078e0800c2c065bbb881485e010dfb3a3eed52a6c55f.php
@@ -0,0 +1,64 @@
+<?php
+
+/* @FOSUser/Security/login.html.twig */
+class __TwigTemplate_61aafd1c44c44c735a74a3ea2f60240f063dfdb3e209142112058f7d470b7b01 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("@FOSUser/layout.html.twig", "@FOSUser/Security/login.html.twig", 1);
+        $this->blocks = array(
+            'fos_user_content' => array($this, 'block_fos_user_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "@FOSUser/layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_fos_user_content($context, array $blocks = array())
+    {
+        // line 4
+        echo "    ";
+        echo twig_include($this->env, $context, "@FOSUser/Security/login_content.html.twig");
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@FOSUser/Security/login.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "@FOSUser/Security/login.html.twig", "C:\\wamp64\\www\\PIDEV\\src\\UserBundle\\Resources\\views\\Security\\login.html.twig");
+    }
+}
